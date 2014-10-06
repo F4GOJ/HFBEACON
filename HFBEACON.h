198c415eb59339c80daed49107f35d55567a0452
Christophe Caiveau <f4goj@free.fr>
2014-10-06 05:40:48 +0200

    Update HFBEACON.h

--- HFBEACON.h
+++ HFBEACON.h
@@ -18,23 +18,23 @@
 
 class HFBEACON
 {
-    public:
-        HFBEACON();
+  public:
+    HFBEACON();
         
-        void sendRsid(long freqRsid, int modeRsid);
-		void cwTx(long freqCw, char * stringCw, int cwWpm);
-        void pskTx(long freqPsk, char * stringPsk, int modePsk, int baudsPsk);
-        void rttyTx(long freqRtty, char * stringRtty);
-		void hellTx(long freqHell, char * stringHell);
-		void wsprTx(long freqWspr, char * callWsprTx, char * locWsprTx, char * powWsprTx);
+   void sendRsid(long freqRsid, int modeRsid);
+   void cwTx(long freqCw, char * stringCw, int cwWpm);
+   void pskTx(long freqPsk, char * stringPsk, int modePsk, int baudsPsk);
+   void rttyTx(long freqRtty, char * stringRtty);
+   void hellTx(long freqHell, char * stringHell);
+   void wsprTx(long freqWspr, char * callWsprTx, char * locWsprTx, char * powWsprTx);
 			
-	private:
-	    void pskIdle(long freqIdle, int baudsIdle);
-		void rttyTxByte (long freqRttyTxbyte, char c);
-		void wsprProcess(char * callWsprProc, char * locWsprProc, char * powWsprProc);
-		byte parity(unsigned long tempo);
-		byte Symbol[163];
-		int wsprSymbGen;
+  private:
+   void pskIdle(long freqIdle, int baudsIdle);
+   void rttyTxByte (long freqRttyTxbyte, char c);
+   void wsprProcess(char * callWsprProc, char * locWsprProc, char * powWsprProc);
+   byte parity(unsigned long tempo);
+   byte Symbol[163];
+   int wsprSymbGen;
 };
 
 extern HFBEACON Beacon;
