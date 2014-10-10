c76e6ce1e8539bf42e67afad9a4a72e46a9835b2
Christophe Caiveau <f4goj@free.fr>
2014-10-10 15:00:07 +0200

    Update HFBEACON.h

--- HFBEACON.h
+++ HFBEACON.h
@@ -21,7 +21,6 @@ class HFBEACON
   public:
     HFBEACON();
         
-   void sendRsid(long freqRsid, int modeRsid);
    void cwTx(long freqCw, char * stringCw, int cwWpm);
    void bpsk31Tx(long freqBpsk31, char * stringBpsk31);
    void qpsk31Tx(long freqQpsk31, char * stringQpsk31);
@@ -33,14 +32,16 @@ class HFBEACON
    void hellTx(long freqHell, char * stringHell);
    void wsprTx(long freqWspr, char * callWsprTx, char * locWsprTx, char * powWsprTx);
    void wsprProcess(char * callWsprProc, char * locWsprProc, char * powWsprProc);
-   byte Symbol[163];
+   byte Symbol[162];
    int wsprSymbGen;
 			
   private:
+   void rsidTx(long freqRsid, int modeRsid);
    void pskIdle(long freqIdle, int baudsIdle);
    void rttyTxByte (long freqRttyTxbyte, char c);
    void pskTx(long freqPsk, char * stringPsk, int modePsk, int baudsPsk);
    byte parity(unsigned long tempo);
+   byte rsidEnable;
 };
 
 extern HFBEACON Beacon;
