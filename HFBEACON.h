65e512771d4851e511ee2243d829ce118203d5b5
Christophe Caiveau <f4goj@free.fr>
2015-01-11 18:57:50 +0100

    Update HFBEACON.h

--- HFBEACON.h
+++ HFBEACON.h
@@ -23,6 +23,7 @@ class HFBEACON
    
    void rsidToggle(boolean rsidEnable);
    void cwTx(long freqCw, char * stringCw, int cwWpm);
+   void pskTx(long freqPsk, char * stringPsk, int modePsk, int baudsPsk);
    void bpsk31Tx(long freqBpsk31, char * stringBpsk31);
    void qpsk31Tx(long freqQpsk31, char * stringQpsk31);
    void bpsk63Tx(long freqBpsk63, char * stringBpsk63);
@@ -43,7 +44,7 @@ class HFBEACON
    void rsidTx(long freqRsid, int modeRsid);
    void pskIdle(long freqIdle, int baudsIdle);
    void rttyTxByte (long freqRttyTxbyte, char c);
-   void pskTx(long freqPsk, char * stringPsk, int modePsk, int baudsPsk);
+   
    byte parity(unsigned long tempo);
    byte rsidTxEnable;
 };
